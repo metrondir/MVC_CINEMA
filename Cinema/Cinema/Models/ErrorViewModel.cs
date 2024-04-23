db1a37e91938da3caf7a015df2f73a4745ef8b6c
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-04-23 14:31:02 +0200

    @

---
+++ Cinema/Cinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
