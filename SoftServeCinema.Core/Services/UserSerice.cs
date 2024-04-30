fb2384d7546a2ce41e8d55889981416e5bb857bd
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2024-04-30 10:38:45 +0300

    adding login

---
+++ SoftServeCinema.Core/Services/UserSerice.cs
@@ -0,0 +1,13 @@
+﻿using SoftServeCinema.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeCinema.Core.Services
+{
+    internal class UserSerice : IUserService
+    {
+    }
+}
